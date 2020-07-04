79aa7da00373b0afce1b15a75e996e828685ed09
sagarvitthalsawant <sagarsawant2100@gmail.com>
2020-07-04 19:19:15 +0100

    Implemented Swagger 2

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # Springboot-Swagger2
+
+Try 
+Get - localhost:8080
+Post - localhost:8080 (add data to save )
+{
+    "id" : "456",
+    "name" : "Naam",
+    "phone" : "12345678901"
+}
+
+localhost:8080/v2/api-docs
+
+http://localhost:8080/swagger-ui.html
---
+++ src/main/java/com/svs/AddressBookResource.java
@@ -0,0 +1,30 @@
+package com.svs;
+
+
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ConcurrentHashMap;
+
+@RestController
+@RequestMapping("/api")
+public class AddressBookResource {
+    ConcurrentHashMap<String, Contact> contacts = new ConcurrentHashMap<>();
+
+    @GetMapping("/{id}")
+    public Contact getContact(@PathVariable String id){
+        return contacts.get(id);
+    }
+
+    @GetMapping("/")
+    public List<Contact> getAllContacts(){
+        return new ArrayList<Contact>(contacts.values());
+    }
+
+    @PostMapping("/")
+    public Contact addContact(@RequestBody Contact contact){
+        contacts.put(contact.getId(), contact);
+        return contact;
+    }
+}
